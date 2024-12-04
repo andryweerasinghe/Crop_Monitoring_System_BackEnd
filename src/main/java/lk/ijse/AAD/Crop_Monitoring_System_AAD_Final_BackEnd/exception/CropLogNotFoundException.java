173021841850da6958da8e90a4b4921ea7777347
andryweerasinghe <andryweerasinghe@gmail.com>
2024-12-04 21:43:43 +0530

    CropLogNotFoundException Implementation

---
+++ src/main/java/lk/ijse/AAD/Crop_Monitoring_System_AAD_Final_BackEnd/exception/CropLogNotFoundException.java
@@ -0,0 +1,9 @@
+package lk.ijse.AAD.Crop_Monitoring_System_AAD_Final_BackEnd.exception;
+
+public class CropLogNotFoundException extends RuntimeException {
+    public CropLogNotFoundException() {}
+    public CropLogNotFoundException(String message) {
+
+    }
+    public CropLogNotFoundException(String message, Throwable cause) {}
+}
