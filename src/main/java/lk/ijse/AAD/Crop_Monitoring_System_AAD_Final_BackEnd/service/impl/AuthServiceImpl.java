bcb0219c224f3acdec1dcaadc24a86907621442b
andryweerasinghe <andryweerasinghe@gmail.com>
2024-12-06 11:56:08 +0530

    Auth ServiceImpl Implementation

---
+++ src/main/java/lk/ijse/AAD/Crop_Monitoring_System_AAD_Final_BackEnd/service/impl/AuthServiceImpl.java
@@ -0,0 +1,11 @@
+/*
+ * Author  : Mr.electrix
+ * Project : Crop_Monitoring_System_AAD_Final
+ * Date    : 12/6/24
+
+ */
+
+package lk.ijse.AAD.Crop_Monitoring_System_AAD_Final_BackEnd.service.impl;
+
+public class AuthServiceImpl {
+}
